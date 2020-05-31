7e03ca2849d8129983d2ec7d371d5c1e57c76f9c
RoberlandRicardo <roberland.ricardo@gmail.com>
2020-05-30 23:41:34 -0300

    Exceptions handling

--- src/main/java/com/example/testeSpring/repositories/CategoryRepository.java
+++ src/main/java/com/example/testeSpring/repositories/CategoryRepository.java
@@ -1,7 +1,5 @@
 package com.example.testeSpring.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.testeSpring.entities.Category;
@@ -9,5 +7,4 @@ import com.example.testeSpring.entities.Category;
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 	
-
 }
